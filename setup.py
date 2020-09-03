116ca12631f0f9f518a056b732667c7cc0e3666c
vars-vlauwen <vincent.lauwen@varsanalytics.com>
2020-09-03 17:22:11 +0200

    chore: update files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     packages=find_packages(),
-    version="0.1.0",
+    version="1.0.0",
     description="CADEX - Explaining Deep Learning Models with Constrained Adversarial Examples",
     author="Jonathan Moore, Nils Hammerla and Chris Watkins",
     license="",
